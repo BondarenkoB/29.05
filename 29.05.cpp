529250b9989e91fc125df586d9d977e2721e86be
Bohdan <bohdanstep23@gmail.com>
2023-05-20 20:46:49 +0300

    done

--- 29.05.cpp
+++ 29.05.cpp
@@ -1,20 +1,41 @@
-﻿// 29.05.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <limits>
+#include <Windows.h>
 
-#include <iostream>
+using namespace std;
 
-int main()
+int main() 
 {
-    std::cout << "Hello World!\n";
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int monthsCount = 12;
+    int profits[monthsCount];
+    int maxProfit = numeric_limits<int>::min();
+    int minProfit = numeric_limits<int>::max();
+    int maxMonth, minMonth;
+
+    cout << "Введіть прибуток фірми за кожен місяць:\n";
+    for (int i = 0; i < monthsCount; i++) 
+    {
+        cout << "Місяць " << i + 1 << ": ";
+        cin >> profits[i];
+
+        if (profits[i] > maxProfit)
+        {
+            maxProfit = profits[i];
+            maxMonth = i + 1;
+        }
+
+        if (profits[i] < minProfit) 
+        {
+            minProfit = profits[i];
+            minMonth = i + 1;
+        }
+    }
+
+    cout << "Місяць з найбільшим прибутком: " << maxMonth << endl;
+    cout << "Місяць з найменшим прибутком: " << minMonth << endl;
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
